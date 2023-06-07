cfe93d908a78e8eaf58a30022e7df81e6bce2615
oliverpeb <83925702+oliverpeb@users.noreply.github.com>
2023-06-08 00:46:11 +0200

    Grøn fixet

--- Raspberry.py
+++ Raspberry.py
@@ -3,21 +3,21 @@ from sense_hat import SenseHat
 # Initialiserer Sense HAT-objektet
 sense = SenseHat()
 
+
 # Funktion til at indstille farve og tekst baseret på temperatur
 def set_led_color_and_text(temperature):
-    if temperature == 19:
-        # Grøn farve
-        sense.clear(0, 255, 0)
-        sense.show_message(" ")
-    elif temperature > 19:
+    if temperature > 19:
         # Rød farve
         sense.clear(255, 0, 0)
         if temperature > 22:
             sense.show_message("Skru ned")
-    else:
+    elif temperature < 19:
         # Blå farve
         sense.clear(0, 0, 255)
-        sense.show_message(" ")
+    else:
+        # Grøn farve
+        sense.clear(0, 255, 0)
+
 
 # Simulerer en temperaturværdi (ændre værdien efter behov)
 temperature = 23
